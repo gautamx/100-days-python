59484e396ec853777c69a7be7c0abba0d2363110
Gautam <gautamratanpuri1311@gmail.com>
2025-02-13 08:43:25 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,28 @@
+# math operators
+
+m1 = int(input("operand1: "))
+m2 = int(input("operand2: "))
+
+sum = m1+m2
+print("The sum of",m1,"and",m2,"is",sum)
+
+diff = m1-m2
+print("The difference between",m1,"and",m2,"is",diff)
+
+prod = m1*m2
+print("The product of",m1,"and",m2,"is",prod)
+
+div = m1/m2
+print("The division of",m1,"by",m2,"gives",div)
+
+floor_div = m1//m2
+print("The floor division of",m1,"by",m2,"gives",floor_div)
+
+mod = m1%m2
+print("The division of",m1,"by",m2,"gives a remainder of",mod)
+
+exp = m1**m2
+print(m1,"raised to",m2,"gives",exp)
+
+
+print("\n")
