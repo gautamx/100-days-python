fe2a665abc58e40b02a4ce408f8111388737d411
ENPHASEENERGY\rgautam <rgautam@enphaseenergy.com>
2025-02-13 16:13:29 +0530

    input function

---
+++ input.py
@@ -0,0 +1,14 @@
+# input function takes the input & gives a return value 
+# as string/character hence we have to pass that into a variable
+a = input("Enter your name: ")
+print("Your name is", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+# input is string, + concatenates
+print(x + y,"concatenation")
+# typecast as int to add
+print("sum of x and y is", int(x) + int(y))
+
+# x = int(input("Enter first number: "))
+# y = int(input("Enter second number: "))
