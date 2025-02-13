f91fa9435100c0b894306f341e9db4dcf7205450
ENPHASEENERGY\rgautam <rgautam@enphaseenergy.com>
2025-02-13 15:48:31 +0530

    implicit typecasting

--- typecasting.py
+++ typecasting.py
@@ -4,5 +4,22 @@ string = "15"
 number = 7
 #sum = string + number     #throws an error because different datatype
 string_number = int(string) #throws an error if the string is not a valid integer
-sum= number + string_number
+sum = number + string_number
 print("The Sum of both the numbers is: ", sum)
+
+
+# implicit typecasting
+
+# Python automatically converts
+# a to int
+a = 7
+print(type(a))
+ 
+# Python automatically converts b to float
+b = 3.0
+print(type(b))
+ 
+# Python automatically converts c to float as it is a float addition
+c = a + b
+print(c)
+print(type(c))
