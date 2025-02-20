7a31c4b6f483839b0cfa929a51b7d9eb8bc220d8
ENPHASEENERGY\rgautam <rgautam@enphaseenergy.com>
2025-02-20 19:09:54 +0530

    if else shorthand

--- if-else.py
+++ if-else.py
@@ -42,3 +42,29 @@ elif (num > 0):
         print("Number is greater than 20")
 else:
     print("Number is zero")
+
+
+
+###########################################################
+# if-else shorthand
+# can be used when the condition being tested is simple 
+# and the code blocks to be executed are short.
+a = 10
+b = 30
+print('a is greater') if ( a > b ) else print('b is greater')
+
+# multiple else statements on the same line
+print('a is greater than') if ( a > 15 ) else print ('a is equal to 15') if ( a==b ) else print('a is less than 15')
+
+
+# another usecase
+
+# result = value_if_true if condition else value_if_false
+c = 100 if ( a > b ) else 17
+print(c)
+
+# same as
+# if condition:
+#     result = value_if_true
+# else:
+#     result = value_if_false
