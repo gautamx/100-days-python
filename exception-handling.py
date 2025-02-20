e4f21611f5215427adcb787e5f989bdb9ea251a4
ENPHASEENERGY\rgautam <rgautam@enphaseenergy.com>
2025-02-20 14:51:29 +0530

    finally keyword, custom error

--- exception-handling.py
+++ exception-handling.py
@@ -25,3 +25,61 @@ except IndexError:
 
 print("Some more imp lines of code")
 print("End of program")
+
+
+# Finally Clause
+# The finally code block is also a part of exception handling. 
+# When we handle exception using the try and except block, 
+# we can include a finally block at the end. 
+
+# The finally block is always executed
+
+# so it is generally used for doing the concluding tasks 
+# like closing file resources
+try:
+    num = int(input("Enter an integer: "))
+except ValueError:
+    print("Number entered is not an integer.")
+else:
+    print("Integer Accepted.")
+finally:
+    print("This block is always executed.")
+
+
+def func1():
+  try:
+    l = [1, 5, 6, 7]
+    i = int(input("Enter the index: "))
+    print(l[i])
+    return 1
+  except:
+    print("Some error occurred")
+    return 0
+
+  finally:
+    print("I am always executed")
+
+x = func1()
+print(x)
+
+
+# Raising Custom Errors
+# using the 'raise' keyword
+salary = int(input('Enter salary amount: '))
+if not 2000 < salary < 5000:
+    raise ValueError("Not a valid salary")
+
+# Defining Custom Exceptions
+# we can define custom exceptions by creating a new class 
+# that is derived from the built-in Exception class.
+# 
+# syntax:
+# class CustomError(Exception):
+#   # code ...
+#   pass
+
+# try:
+#   # code ...
+
+# except CustomError:
+#   # code...
