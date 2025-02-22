715eae1516e943186e1fdc7f9b8a196253e5217c
Gautam <gautamratanpuri1311@gmail.com>
2025-02-22 18:39:33 +0530

    seek and tell functions

--- file-io.py
+++ file-io.py
@@ -87,3 +87,34 @@ while True:
     print(f"Marks of student {i} in Physics is: {m2}")
     print(f"Marks of student {i} in Chemistry is: {m3}")
 f.close()
+
+
+
+#########################################################################
+# seek() and tell() functions are used to work with file objects and their positions within a file
+
+# seek() function allows you to move the current position within a file to a specific point. 
+# The position is specified in bytes, and you can move either forward or backward from the current position
+with open('file2.txt', 'r') as f:
+    # Move to the 10th byte in the file
+    f.seek(10)
+    # Read the next 5 bytes
+    data = f.read(50)
+    print(data)
+
+# tell() function returns the current position within the file, in bytes. 
+# This can be useful for keeping track of your location within the file 
+# or for seeking to a specific position relative to the current position.
+    # Save the current position
+    current_position = f.tell()
+    # seek the current position
+    print(current_position)
+    print( f.seek(current_position) )
+    f.close()
+
+# with open('file2.txt', 'w') as f:
+#     f.seek(10)
+#     # if you want to truncate the file to a specific size, you can use the truncate function.
+#     f.truncate(200)       # can be done in 'w' or 'a' mode
+#     print(f.read)
+#     f.close()
