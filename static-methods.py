200b4a4af5541d2fd9a33890ea8964300f2c1e0a
Gautam <gautamratanpuri1311@gmail.com>
2025-02-26 08:20:44 +0530

    static methods in OOP

---
+++ static-methods.py
@@ -0,0 +1,22 @@
+# Static methods in Python are methods that belong to a class rather than an instance of the class. 
+# They are defined using the @staticmethod decorator
+# Static methods are often used to create utility functions that don't need access to instance data.
+class Math:
+    @staticmethod
+    def add(*args):
+        sum=0
+        for i in range(len(args)):
+            sum += int(args[i])
+        return sum
+
+print(Math.add(10,10,10,10,3))
+
+# In this example, the add method is a static method of the Math class.
+# The method can be called on the class itself, without the need to create an instance of the class.
+
+# static methods shouldn't have a 'self' parameter since they don't have access to instance attributes.
+
+# In Python, static methods can be called both from the class itself and from any instance of the class.
+
+inst = Math()
+print(inst.add(4,5))
